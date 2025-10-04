c88d6371c77bf35cf0c5b84c7cac2e1c923537b8
sami el amoudi <120538472+samstechc0de@users.noreply.github.com>
2025-10-03 21:34:49 -0700

    Create background.js

---
+++ extension/background.js
@@ -0,0 +1,7 @@
+// background service worker for Pumpredict extension
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+  // Activation handler
+});
